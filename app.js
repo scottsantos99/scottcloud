260c506d559ae53acdf01ca45d58850d8fbdb1a5
scottsantos99 <scott.santos@gmail.com>
2017-06-17 23:18:37 -0700

    adding app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express'),
+    mongoose = require('mongoose');
+
+var connectionOptions = {
+    user: 'serviceUser',
+    pass: 'Qpmz123$'
+};
+
+var db = mongoose.connect('mongod://ec2-13-58-226-34.us-east-2.compute.amazonaws.com:27017/test',
+    connectionOptions);
+
+console.log(mongoose.connection.readyState);
+
+var restaurant = require('./models/restaurantModel');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+var restaurantRouter = express.Router();
+
+restaurantRouter.route('/restaurant')
+    .get(function(req, res){
+        restaurant.find(function(error, restaurants){
+            console.log('starting restaurant find');
+            if(err) console.log(err);
+            else res.json(restaurants);
+        }).
+        limit(10);
+    });
+
+app.use('/api', restaurantRouter);
+
+app.get('/', function(req, res) {
+    res.send('welcome to my API!');
+});
+
+app.listen(port, function() {
+    console.log('Gulp is running on PORT: ' + port);
+});
