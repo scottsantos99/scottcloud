477293d05d3d9f0e78a98c83c5e0ad3bfd6bf2dc
scottsantos99 <scott.santos@gmail.com>
2017-07-02 16:29:14 -0700

    changed dns for db

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var connectionOptions = {
     pass: 'Qpmz123$'
 };
 
-var db = mongoose.connect('mongodb://ec2-52-15-192-24.us-east-2.compute.amazonaws.com:27017/test',
+var db = mongoose.connect('mongodb://ec2-13-59-15-56.us-east-2.compute.amazonaws.com:27017/test',
     connectionOptions, function(err) {
     console.log('tried to connect, but...' + err);
 });
