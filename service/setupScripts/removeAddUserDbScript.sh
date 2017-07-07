e0eaf11bc4d96a6125b3261bfab094208e037a31
scottsantos99 <scott.santos@gmail.com>
2017-07-07 13:48:21 -0700

    removing setupScripts from service folder

--- service/setupScripts/removeAddUserDbScript.sh
+++
@@ -1,6 +0,0 @@
-echo -e "\n\nRemove and add db user"
-
-./mongodb/bin/mongo --eval 'db.dropUser("serviceUser")'
-./mongodb/bin/mongo --eval 'db.createUser( { user: "serviceUser", pwd: "Qpmz123$", roles: [ { role: "readWrite", db: "test" } ] } )'
-
-echo -e "\n\nDone removing and adding db user"
--- service/setupScripts/stopDbScript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#echo -e "\n\nstarting mongo shell"
-#./mongodb/bin/mongo --eval "db.stats()"
-#statsResult=$?
-#echo -e "$statsResult"
-./mongodb/bin/mongo --eval "db=db.getSiblingDB('admin');db.shutdownServer()"
-echo "done stopping db"
