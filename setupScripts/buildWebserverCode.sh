054d3b1fa96ae8a4d2b986a641ff2bc38b3ea19e
scottsantos99 <scott.santos@gmail.com>
2017-07-10 22:36:42 -0700

    updating setup scripts and changed the build output directory

--- setupScripts/buildWebserverCode.sh
+++ setupScripts/buildWebserverCode.sh
@@ -2,3 +2,13 @@
 
 cd ~/VisualStudio/scottcloud/webserver
 npm run build
+cd dist
+mkdir server
+cp ../server.js ./server
+cd ..
+rm dist.zip
+zip -1 -r dist.zip dist
+git remote set-url origin git@github.com:scottsantos99/scottcloud.git
+git add dist.zip
+git commit -m 'auto checkin of prod files'
+git push
