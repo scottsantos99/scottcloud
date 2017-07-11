af09d7f6d86fc81581755b0d48393d6f0d91e525
scottsantos99 <scott.santos@gmail.com>
2017-07-10 22:37:49 -0700

    adding server.js

---
+++ webserver/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var app = express();
+
+var port = 5000;
+
+var path = require('path');
+
+var clientFilesDir = path.join(__dirname, "dist/client");
+
+app.use(express.static(clientFilesDir));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(clientFilesDir + '/index.html'));
+});
+
+app.listen(port, function(err) {
+    console.log('running of port:' + port);
+})
